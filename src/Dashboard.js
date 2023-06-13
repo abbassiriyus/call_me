aa47fa7bca75915c17f64274ff0f637cf95a1a27
Flander404 <109587664+Flander404@users.noreply.github.com>
2023-06-13 21:07:51 +0500

    fsdg
    dfgf

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,6 +3,14 @@ import logo from "./nimg/Logo_color 1.png";
 import "./style/Dashboard.css";
 import Accordion from "react-bootstrap/Accordion";
 import { GoBriefcase } from "react-icons/go";
+import { CiSaveDown2 } from "react-icons/ci";
+import { BsClipboardCheck } from "react-icons/bs";
+import { BsChatLeft } from "react-icons/bs";
+import { BsTelephone } from "react-icons/bs";
+import { HiOutlineUserGroup } from "react-icons/hi";
+import { HiOutlineLogout } from "react-icons/hi";
+import { HiOutlineCreditCard } from "react-icons/hi";
+import { HiOutlineChartSquareBar } from "react-icons/hi";
 
 class Dashboard extends Component {
   render() {
@@ -20,25 +28,92 @@ class Dashboard extends Component {
               <Accordion defaultActiveKey={["0"]} alwaysOpen>
                 <Accordion.Item eventKey="0">
                   <Accordion.Header>
-                    <GoBriefcase /> <p>Рабочий стол</p> 
+                    <GoBriefcase /> <p>Рабочий стол</p>
                   </Accordion.Header>
                   <Accordion.Body>
                     <div className="body-down">
-                      <GoBriefcase /> Рабочий стол
+                      <CiSaveDown2 /> Настройки
                     </div>
                   </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
-                  <Accordion.Header>Accordion Item #2</Accordion.Header>
+                  <Accordion.Header>
+                    <BsClipboardCheck /> <p>Онлайн - запись</p>
+                  </Accordion.Header>
                   <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
+                    <div className="body-down">
+                      <CiSaveDown2 /> Настройки
+                    </div>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                  <Accordion.Header>
+                    <HiOutlineUserGroup /> <p>Клиенты</p>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <div className="body-down">
+                      <CiSaveDown2 /> Настройки
+                    </div>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="3">
+                  <Accordion.Header>
+                    <HiOutlineChartSquareBar /> <p>Отчеты</p>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <div className="body-down">
+                      <CiSaveDown2 /> Настройки
+                    </div>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="4">
+                  <Accordion.Header>
+                    <BsTelephone /> <p>Обратный звонок</p>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <div className="body-down">
+                      <CiSaveDown2 /> Настройки
+                    </div>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="5">
+                  <Accordion.Header>
+                    <BsChatLeft /> <p>Чат на сайт</p> 
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <div className="body-down">
+                      <CiSaveDown2 /> Настройки
+                    </div>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="6">
+                  <Accordion.Header>
+                    <HiOutlineCreditCard /> <p>Тарифы</p> 
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <div className="body-down">
+                      <CiSaveDown2 /> Настройки
+                    </div>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="7">
+                  <Accordion.Header>
+                    <GoBriefcase /> <p>Настройки</p> 
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <div className="body-down">
+                      <CiSaveDown2 /> Настройки
+                    </div>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="8">
+                  <Accordion.Header>
+                    <HiOutlineLogout /> <p>Выход</p> 
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <div className="body-down">
+                      <CiSaveDown2 /> Настройки
+                    </div>
                   </Accordion.Body>
                 </Accordion.Item>
               </Accordion>
